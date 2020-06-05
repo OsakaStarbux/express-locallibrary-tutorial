3e3d5c232e010f21ae54417caca1cbd8b37bb2d0
Kevin Bradley <kevin.bradley0928@gmail.com>
2020-06-05 11:14:08 +0900

    Require catalogRouter in app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose')
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
 
 const app = express();
 

237ff114c2ea4fe06c2c1b25405633c70085d574
Kevin Bradley <kevin.bradley0928@gmail.com>
2020-06-10 11:26:05 +0900

    Update book_delete_post controller function

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -187,7 +187,30 @@ exports.book_delete_get = function(req, res, next) {
 
 // Handle book delete on POST.
 exports.book_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book delete POST');
+  async.parallel({
+      book: function(callback) {
+        Book.findById(req.body.bookid).exec(callback)
+      },
+      book_instances: function(callback) {
+        BookInstance.find({ 'book': req.body.bookid }).exec(callback)
+      },
+  }, function(err, results) {
+      if (err) { return next(err); }
+      // Success
+      if (results.book_instances.length > 0) {
+          // Author has books. Render in same way as for GET route.
+          res.render('book_delete', { title: 'Delete Book', book: results.book, book_instances: results.book_instances } );
+          return;
+      }
+      else {
+          // Author has no books. Delete object and redirect to the list of authors.
+          Book.findByIdAndRemove(req.body.bookid, function deleteBook(err) {
+              if (err) { return next(err); }
+              // Success - go to book list
+              res.redirect('/catalog/books')
+          })
+      }
+  })
 };
 
 // Display book update form on GET.
